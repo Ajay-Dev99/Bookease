5c4ffc3f110ffa963161a67bf33cbbb3fadb9720
ajaydev <ajaydevkomath@gmail.com>
2026-02-13 22:54:52 +0530

    feat: Implement customer login page with routing and UI components.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import UserLayout from './layouts/UserLayout';
 
+import CustomerLogin from './pages/CustomerLogin';
+
 function App() {
   return (
     <Router>
@@ -10,6 +12,7 @@ function App() {
         <Route element={<UserLayout />}>
           <Route path="/" element={<Home />} />
         </Route>
+        <Route path="/login/customer" element={<CustomerLogin />} />
       </Routes>
     </Router>
   );
